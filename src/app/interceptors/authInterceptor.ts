e90e16bcf76f3fbbd703d63b77f2945ed81db90c
jonatha <al221310334@gmail.com>
2017-12-15 11:00:58 -0600

    observable

--- src/app/interceptors/authInterceptor.ts
+++ src/app/interceptors/authInterceptor.ts
@@ -3,14 +3,14 @@
  */
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+ 
 
 
 @Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
  
-  intercept (req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+  intercept (req: HttpRequest<any>, next: HttpHandler):any  {
 
     
      
